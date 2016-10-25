01acd0ee2d2e9e2d4a4aea05abb6a0e2301797ea
MinJu Kim <kimminju_99@naver.com>
2016-10-25 20:11:59 +0900

    #3 Event Programming

--- app/src/main/java/kr/hs/emirim/ksmj0211/gallerytest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ksmj0211/gallerytest/MainActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.ksmj0211.gallerytest;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -42,8 +43,17 @@ public class MainActivity extends AppCompatActivity {
             ImageView imgV = new ImageView(context);
             imgV.setLayoutParams(new Gallery.LayoutParams(200, 300));
             imgV.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            imgV.setPadding(5, 5, 5, 5);
+            //imgV.setPadding(5, 5, 5, 5);
             imgV.setImageResource(posterIDs[position]);
+            final int pos = position;
+            imgV.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    imgMain.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    imgMain.setImageResource(posterIDs[pos]);
+                    return false;
+                }
+            });
             return imgV;
         }
         public Object getItem(int position){
